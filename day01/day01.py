4daf3467ee82b7b890f9f2c0ec71f1adcaea207e
Daniel Wong <dsw2107@gmail.com>
2015-12-02 18:45:27 -0500

    initial commit
    completed day 1, working on day 2

---
+++ day01/day01.py
@@ -0,0 +1,17 @@
+with open("day01.txt", "r") as myfile:
+	day01_input = myfile.read().replace('\n', '')
+
+day01_list = list(day01_input)
+
+floor = 0
+
+for index in range(len(day01_list)):
+	if index == 0:
+		floor = 0
+	if day01_list[index] == '(':
+		floor += 1
+	elif day01_list[index] == ')':
+		floor -= 1
+	if floor == -1:
+		print (index + 1)
+
---
+++ day02/day02.py
@@ -0,0 +1,17 @@
+with open("day02_input.txt", "r") as myfile:
+	day02_input = (line.rstrip('\n').replace('x','*') for line in myfile.read())
+
+print day02_input
+
+
+
+
+
+
+
+
+
+
+
+
+
